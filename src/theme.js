d683964d1114d305d11b5ea93641aec310c6b499
ran <skyran1278@gmail.com>
2021-07-24 16:30:59 +0800

    change theme color

--- src/theme.js
+++ src/theme.js
@@ -3,17 +3,11 @@ import { createTheme } from '@material-ui/core/styles';
 // A custom theme for this app
 const theme = createTheme({
   palette: {
-    // #f8f9fa
     primary: {
-      light: '#484848',
-      main: '#212121',
-      dark: '#000000',
+      main: '#f8f9fa',
     },
-    // #0b7bf1
     secondary: {
-      light: '#4dabf5',
-      main: '#2196f3',
-      dark: '#1769aa',
+      main: '#0b7bf1',
     },
   },
 });
