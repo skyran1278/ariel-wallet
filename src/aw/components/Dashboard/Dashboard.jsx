7bc72b8c52e67b7d6d60a10a292191be0299403c
ran <skyran1278@gmail.com>
2021-07-25 08:28:54 +0800

    init Dashboard component with chart.js

---
+++ src/aw/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,57 @@
+import Chart from 'chart.js/auto';
+import React, { useEffect, useRef } from 'react';
+
+function Dashboard() {
+  const chartElement = useRef(null);
+
+  useEffect(() => {
+    const labels = ['Food', 'Drink', 'March', 'April', 'May', 'June'];
+
+    const config = {
+      labels,
+      datasets: [
+        {
+          label: 'Dashboard',
+          data: [10, 20, 30, 40, 50, 60],
+        },
+      ],
+    };
+
+    const options = {
+      indexAxis: 'y',
+      // Elements options apply to all of the options unless overridden in a dataset
+      // In this case, we are setting the border of each horizontal bar to be 2px wide
+      // elements: {
+      //   bar: {
+      //     borderWidth: 2,
+      //   }
+      // },
+      // responsive: true,
+      // plugins: {
+      //   legend: {
+      //     position: 'right',
+      //   },
+      //   title: {
+      //     display: true,
+      //     text: 'Chart.js Horizontal Bar Chart'
+      //   }
+      // }
+    };
+
+    const chart = new Chart(chartElement.current, {
+      type: 'bar',
+      data: config,
+      options,
+    });
+    chart.render();
+  }, []);
+  return (
+    <>
+      <canvas ref={chartElement} />
+    </>
+  );
+}
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
