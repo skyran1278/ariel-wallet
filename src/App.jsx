3a5e3f8f36bdb9abefb56549c828a7c139e57341
ran <skyran1278@gmail.com>
2021-07-25 08:30:26 +0800

    init App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from 'aw/pages/Home';
+import SignIn from 'aw/pages/SignIn';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <SignIn />
+      <Home />
+    </>
+  );
+}
+
+App.propTypes = {};
+
+export default App;
