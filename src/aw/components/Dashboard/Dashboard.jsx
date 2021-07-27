3023e60b40edf1c1d60e54ffea7580d454b48fa0
ran <skyran1278@gmail.com>
2021-07-27 17:03:47 +0800

    still meet error Canvas is already in use. Chart with ID '0' must be destroyed before the canvas can be reused.

--- src/aw/components/Dashboard/Dashboard.jsx
+++ src/aw/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,45 @@
+import dataset from 'aw/dataset';
 import Chart from 'chart.js/auto';
-import { useEffect, useRef } from 'react';
+import { groupBy, mapValues } from 'lodash';
+import { useEffect, useRef, useState } from 'react';
+import useSWR from 'swr';
 
+// function useCategories() {
+//   const { data, error } = useSWR(`/api/categories`, () =>
+//     Promise.resolve(dataset.categories)
+//   );
+
+//   return {
+//     categories: data,
+//     isLoading: !error && !data,
+//     isError: error,
+//   };
+// }
+
+// function useRecords() {
+//   const { data, error } = useSWR(`/api/records`, () =>
+//     Promise.resolve(dataset.records)
+//   );
+
+//   return {
+//     records: data,
+//     isLoading: !error && !data,
+//     isError: error,
+//   };
+// }
+
+// https://dev.to/vcanales/using-chart-js-in-a-function-component-with-react-hooks-246l
+// https://www.createwithdata.com/react-chartjs-dashboard/
+// { categories, records }
 function Dashboard() {
   const chartElement = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+  const { data: categories } = useSWR(`/api/categories`, () =>
+    Promise.resolve(dataset.categories)
+  );
+  const { data: records } = useSWR(`/api/records`, () =>
+    Promise.resolve(dataset.records)
+  );
 
   useEffect(() => {
     const labels = ['Food', 'Drink', 'March', 'April', 'May', 'June'];
@@ -13,6 +50,7 @@ function Dashboard() {
         {
           label: 'Dashboard',
           data: [10, 20, 30, 40, 50, 60],
+          borderWidth: 1,
         },
       ],
     };
@@ -38,13 +76,54 @@ function Dashboard() {
       // }
     };
 
-    const chart = new Chart(chartElement.current, {
-      type: 'bar',
-      data: config,
-      options,
-    });
-    chart.render();
-  }, []);
+    if (chartElement?.current) {
+      // Canvas is already in use. Chart with ID '0' must be destroyed before the canvas can be reused.
+      console.log('hi');
+      const newChartInstance = new Chart(chartElement.current, {
+        type: 'bar',
+        data: config,
+        options,
+      });
+      setChartInstance(newChartInstance);
+
+      return () => newChartInstance.destroy();
+    }
+    return () => {};
+  }, [chartElement]);
+
+  useEffect(() => {
+    if (!chartInstance || !records || !categories) {
+      return;
+    }
+
+    const labels = categories
+      .filter((category) => category.type === 'EXPENSE')
+      .map((category) => category.name);
+
+    console.log(labels);
+
+    const data = mapValues(
+      groupBy(
+        records.map((record) => ({ ...record })),
+        (record) => record.category
+      ),
+      (v) => v.reduce((sum, record) => sum + record.amount, 0)
+    );
+
+    console.log(data);
+
+    // const labelIds = categories
+    //   .filter((category) => category.type === 'EXPENSE')
+    //   .map((category) => category.id);
+
+    // const data = labelIds.map((label) => dataObject[label] ?? 0);
+
+    chartInstance.data.labels = labels;
+    chartInstance.data.datasets[0].data = data;
+
+    chartInstance.update();
+  }, [chartInstance, records, categories]);
+
   return (
     <>
       <canvas ref={chartElement} />
